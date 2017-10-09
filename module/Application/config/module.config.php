2de89541e16705a60147f63a4318c3a2635fedc9
pawel <pawelklaus@hotmail.de>
2017-10-09 22:40:01 +0200

    - change naming to login ~ lostandfound

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -46,7 +46,7 @@ return [
             'login' => [
                 'type'    => Segment::class,
                 'options' => [
-                    'route'    => '/login[/:action[/:id]]',
+                    'route'    => '/lostandfound[/:action[/:id]]',
                     'defaults' => [
                         'controller' => Controller\AuthController::class,
                         'action'     => 'index',
